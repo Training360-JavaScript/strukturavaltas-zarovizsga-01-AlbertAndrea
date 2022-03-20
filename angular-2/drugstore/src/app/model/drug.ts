db25140eec932f99e6cbf95886012cbe2466f229
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2022-03-20 14:41:42 +0100

    solved late

--- angular-2/drugstore/src/app/model/drug.ts
+++ angular-2/drugstore/src/app/model/drug.ts
@@ -1,9 +1,9 @@
 export class Drug {
-    "id": number = 1;
-    "name": string = "SEscitalopram Oxalate";
-    "brand": string = "Escitalopram";
-    "company"?: string = "Major Pharmaceuticals";
-    "barcode": string = "23-537-0123";
-    "stock": number = 26;
+    id: number = 1;
+    name: string = "SEscitalopram Oxalate";
+    brand: string = "Escitalopram";
+    company?: string = "Major Pharmaceuticals";
+    barcode: string = "23-537-0123";
+    stock: number = 26;
   };
 
